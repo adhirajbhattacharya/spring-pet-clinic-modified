047a5290d9bd166786e65b37c78b02c591eb5fb3
adhirajbhattacharya <adhirajbhattacharya@gmail.com>
2018-10-31 23:47:54 +0530

    first commit

---
+++ src/main/java/com/adhiraj/clinic/SpringPetClinicModifiedApplication.java
@@ -0,0 +1,12 @@
+package com.adhiraj.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicModifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicModifiedApplication.class, args);
+	}
+}
