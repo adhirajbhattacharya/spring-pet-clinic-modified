6eba62b275583b50b2967e3227af332fae6d3643
adhirajbhattacharya <adhirajbhattacharya@gmail.com>
2018-11-03 04:25:20 +0530

    added BaseEntity and inheritance. closes #5

--- spring-pet-clinic-modified-data/src/main/java/com/adhiraj/clinic/model/PetType.java
+++ spring-pet-clinic-modified-data/src/main/java/com/adhiraj/clinic/model/PetType.java
@@ -1,5 +1,15 @@
 package com.adhiraj.clinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+   
 }
