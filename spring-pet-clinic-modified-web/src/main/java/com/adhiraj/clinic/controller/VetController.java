f793433f4530cd068d30a0d97d9d3f73e2358d56
adhirajbhattacharya <adhirajbhattacharya@gmail.com>
2018-11-13 02:02:11 +0530

    listed all dataloader data on vet index page. closes #13

--- spring-pet-clinic-modified-web/src/main/java/com/adhiraj/clinic/controller/VetController.java
+++ spring-pet-clinic-modified-web/src/main/java/com/adhiraj/clinic/controller/VetController.java
@@ -1,15 +1,26 @@
 package com.adhiraj.clinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.adhiraj.clinic.model.service.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+  private final VetService vetService;
+
+  @Autowired
+  public VetController(VetService vetService) {
+    super();
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"", "/index", "/index.html"})
   public String listVets(Model model) {
+    model.addAttribute("vets", vetService.findAll());
     return "vets/vet-index";
   }
 
