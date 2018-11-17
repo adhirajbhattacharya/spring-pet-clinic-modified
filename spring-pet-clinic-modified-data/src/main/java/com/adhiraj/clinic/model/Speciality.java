8d6ad257f0af06a78a650b0eace990bfcf5558ac
adhirajbhattacharya <adhirajbhattacharya@gmail.com>
2018-11-17 16:45:14 +0530

    add specialitites. closes #27

---
+++ spring-pet-clinic-modified-data/src/main/java/com/adhiraj/clinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package com.adhiraj.clinic.model;
+
+public class Speciality extends BaseEntity {
+
+  private static final long serialVersionUID = -6369105622064060027L;
+
+  private String description;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+
+}
